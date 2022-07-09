3cec6a747b71aa86e933224034ec109add5ebc99
Tyler Grinstead <tyler@tgdesign.io>
2022-07-09 16:47:22 -0400

    test build

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import NavigationBar from "@/components/NavigationBar.vue";
+import Footer from "@/components/Footer.vue";
+import MainHero from "@/components/MainHero.vue";
+
+
+</script>
+
+<template>
+  <NavigationBar />
+  <MainHero />
+  <Footer />
+</template>
+
+
+<style scoped>
+
+</style>
