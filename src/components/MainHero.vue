5d482c241ae7506587a521f21b2a037165e32699
Tyler Grinstead <tyler@tgdesign.io>
2022-07-09 18:36:42 -0400

    Updated website title

--- src/components/MainHero.vue
+++ src/components/MainHero.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="hero min-h-screen mt-12" style="background-image: url(https://scontent-ort2-1.xx.fbcdn.net/v/t39.30808-6/279545820_305776225042872_976999550270044706_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=19026a&_nc_ohc=esWFEmTt8woAX_g1byH&_nc_ht=scontent-ort2-1.xx&oh=00_AT_cprKG-tOg6lqfsevnrBlMzzv1vp4GDNU41MVaYl8JiQ&oe=62CF856D);">
+  <div class="hero min-h-screen -mt-12" style="background-image: url(https://scontent-ort2-1.xx.fbcdn.net/v/t39.30808-6/279545820_305776225042872_976999550270044706_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=19026a&_nc_ohc=esWFEmTt8woAX_g1byH&_nc_ht=scontent-ort2-1.xx&oh=00_AT_cprKG-tOg6lqfsevnrBlMzzv1vp4GDNU41MVaYl8JiQ&oe=62CF856D);">
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content">
       <div class="max-w-md">
