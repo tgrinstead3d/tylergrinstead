db55d4c704c9b057a7693ad441fdcc42efdd3c2e
Tyler Grinstead <tyler@tgdesign.io>
2022-07-10 09:22:42 -0400

    Updated style / text

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -10,7 +10,7 @@
         <label tabindex="0" class="btn btn-ghost btn-circle">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
         </label>
-        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral/80 rounded-box w-52">
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100/80 rounded-box w-52">
           <li><a>About Me</a></li>
           <li><a>Portfolio</a></li>
           <li><a>Blog</a></li>
